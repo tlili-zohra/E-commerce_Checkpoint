be7e676ec9d4a7e7521773d17a2b0b12324581e6
Zohra Tlili <tlili.zohra12@gmail.com>
2025-05-20 01:36:42 +0200

    yes

--- mern-ecommerce/frontend/src/api/api.js
+++ mern-ecommerce/frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-commerce-checkpoint.onrender.com/api",
 });
 
 /*API.interceptors.request.use((config) => {
